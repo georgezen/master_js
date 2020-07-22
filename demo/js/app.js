64c331ab643264764068eeb94c41d6662e9325ed
georgezen <georgeutm2014@gmail.com>
2020-07-22 01:14:54 -0500

    uso estricto

---
+++ demo/js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+//esta linea de codigo activa el modo estricto de los estandares de ecma 6 y posteriores
+
+var cachonda = 'te pones bien cachonda';
+var madre = 'hija de tu puta madre';
+
+cachonda = 'perra';
+//se puede sobreescribir los valores iniciales de las variables
+
+console.log('hola pinche putita' + ' ' + cachonda + ' ' + madre);
+document.write('hola pinche putita' + ' ' + cachonda + ' ' + madre);
