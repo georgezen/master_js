dfb8ea47fe86f68fe972e2dbfdd670e4414c1418
georgezen <georgeutm2014@gmail.com>
2020-07-31 22:07:53 -0500

    funciones con parametros opcionales

--- demo/js/funciones-arrays.js
+++ demo/js/funciones-arrays.js
@@ -1,28 +1,38 @@
 "use strict";
 
-
 function calculadora(numero1, nmerero2, operador) {
-    
-    var resultado = 0;
-    switch (operador) {
-        case "+":
-            resultado = numero1 + nmerero2;
-        break;
-        case "-":
-            resultado = numero1 - nmerero2;
-        break;
-        case "*":
-            resultado = numero1 * nmerero2;
-            break;
-        
-        case "/":
-            resultado = numero1 / nmerero2;
-        break;
-        default:
-            console.log('ingrese algo no sea pendejo');
-        break;
-    }
-    return resultado;
+  var resultado = 0;
+  switch (operador) {
+    case "+":
+      resultado = numero1 + nmerero2;
+      break;
+    case "-":
+      resultado = numero1 - nmerero2;
+      break;
+    case "*":
+      resultado = numero1 * nmerero2;
+      break;
+
+    case "/":
+      resultado = numero1 / nmerero2;
+      break;
+    default:
+      console.log("ingrese algo no sea pendejo");
+      break;
+  }
+  return resultado;
 }
 
 document.write(calculadora(2, 4, "/"));
+
+//esta funcion cuenta con parametros opcionales las cuales si las putas envias bien, si no ps a la verga
+function persona(nombre = "", edad = 0) {
+  if (nombre.length > 8 && edad > 18) {
+    console.log(nombre, edad);
+  } else if (nombre == "") {
+    console.log("ponga un puto nombre ");
+  }
+}
+
+persona();
+persona('hola pinche putita',20);
