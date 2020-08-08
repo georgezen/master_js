c2667f4056951c0054f4980e83409d144353a7b2
georgezen <georgeutm2014@gmail.com>
2020-08-07 23:49:06 -0500

    funciones parte 2

--- demo/js/demotexto.js
+++ demo/js/demotexto.js
@@ -7,6 +7,9 @@ var cadena = document.querySelector("#cadena");
 var resultado = document.querySelector(".resultado");
 var tipo = 0;
 var texto_val = "";
+var reemplazar = document.querySelector("#reemplazar");
+var search = document.querySelector("#search");
+var search_string = document.querySelector("#search_string");
 
 mayuscula.addEventListener("click", function (params) {
   texto_val = texto.value;
@@ -26,7 +29,25 @@ cadena.addEventListener("click", function (params) {
   mostrar_texto(texto_val, tipo);
 });
 
+search_string.addEventListener("click", function (params) {
+  texto_val = texto.value;
+  
+  tipo = 4;
+  mostrar_texto(texto_val, tipo);
+});
+
+reemplazar.addEventListener("click", function (params) {
+  texto_val = texto.value;
+
+  tipo = 5;
+  mostrar_texto(texto_val, tipo);
+});
+
 function mostrar_texto(texto_val, tipo) {
+  if (texto_val == "") {
+    texto_val = "ponga texto perro";
+    resultado.innerHTML = texto_val;
+  }
   switch (tipo) {
     case 1:
       resultado.innerHTML = texto_val.toUpperCase();
@@ -38,6 +59,16 @@ function mostrar_texto(texto_val, tipo) {
 
     case 3:
       resultado.innerHTML = texto_val.length;
+      break;
+    case 4:
+      //parte de busqueda de caracteres string
+      resultado.innerHTML = texto_val.includes(search.value);
+
+      break;
+    case 5:
+      //parte de reemplazo de caracteres string o cadenas enteras de texto
+      resultado.innerHTML = texto_val.replace(texto_val,"212121");
+
       break;
 
     default:
