1c691ab3a45f21a552646b75706fa71850618777
georgezen <georgeutm2014@gmail.com>
2020-08-12 01:10:44 -0500

    mas arrays y preparacion segundo bloque ejercicios

--- demo/js/arrays.js
+++ demo/js/arrays.js
@@ -1,8 +1,21 @@
-'use strict'
+"use strict";
 
-var array1 = ['212121', '212121', 4, 2121];
+var array1 = ["212121", "212121", 4, 2121];
 
 for (let index = 0; index < array1.length; index++) {
-    document.write("<h3>" + array1[index] + "</h3>");
-    
+  document.write("<h3>" + array1[index] + "</h3>");
 }
+
+var array2 = ["212121", "21212121w", 4, 2121, "2121212223E", true, array1,'dona pelos'];
+
+
+// do {
+// var  adicionado = prompt("ingrese el siguiente elemento del array");
+//   array2.push(adicionado); // el .push añade en el ultimo indice otro elemento al array
+// } while (adicionado != "fin");
+
+// array2.pop();//este metodo el .pop borra el ultimo elemento de un array
+// console.log(array2);
+
+console.log(array2.sort());//este metodo ordena un array en orden alfabetico
+
