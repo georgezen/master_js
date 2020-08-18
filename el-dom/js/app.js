88608487abd8a5bf1756bbcad99893dd459015b9
georgezen <georgeutm2014@gmail.com>
2020-08-18 02:21:40 -0500

    creacion de elementos dom y un poco de bom

--- el-dom/js/app.js
+++ el-dom/js/app.js
@@ -3,11 +3,22 @@
 
 var presioname = document.querySelector("#presioname");
 var texto = document.querySelector("#texto");
+var llamadoTexto1 = document.querySelector("#llamadoTexto");
+var recarga = document.querySelector("#recarga");
+
 presioname.addEventListener('click', () => llamadoTexto());
 
-function llamadoTexto() {
-    setTimeout(() => {
-        texto.innerHTML += "hola" + "\n";    
-    }, 5000);
+recarga.addEventListener("click", () => recargaPagina());
+
+var llamadoTexto = () => {
     
-}
+    var texto1 = document.createTextNode("hola pinche putita" + "\n");
+    llamadoTexto1.appendChild(texto1);
+
+};
+
+
+//Esta parte es para manejar el bom, o sea la ventana del navegador
+var recargaPagina = () => window.location.reload();
+
+
