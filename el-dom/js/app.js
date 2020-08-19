718793314a39a4f420d23ccde7863003fc5da067
georgezen <georgeutm2014@gmail.com>
2020-08-19 01:36:58 -0500

    mas practica del dom

--- el-dom/js/app.js
+++ el-dom/js/app.js
@@ -1,24 +1,39 @@
 'use strict';
 
+window.addEventListener('load', () => {
+    var presioname = document.querySelector("#presioname");
+    var texto = document.querySelector("#texto");
+    var llamadoTexto1 = document.querySelector("#llamadoTexto");
+    var recarga = document.querySelector("#recarga");
+    var mostrar_form = document.querySelector("#mostrar_form");
+    var form = document.querySelector("#form");
+    var nombre = document.querySelector("#nombre");
 
-var presioname = document.querySelector("#presioname");
-var texto = document.querySelector("#texto");
-var llamadoTexto1 = document.querySelector("#llamadoTexto");
-var recarga = document.querySelector("#recarga");
+    presioname.addEventListener("click", () => llamadoTexto());
+    recarga.addEventListener("click", () => recargaPagina());
+    mostrar_form.addEventListener("click", () => mostrar_form1());
+    nombre.addEventListener('keyup', () => calcando_texto());
 
-presioname.addEventListener('click', () => llamadoTexto());
+    var llamadoTexto = () => {
+      var texto1 = document.createTextNode("hola pinche putita");
+      llamadoTexto1.appendChild(texto1);
+    };
 
-recarga.addEventListener("click", () => recargaPagina());
+    //Esta parte es para manejar el bom, o sea la ventana del navegador
+    var recargaPagina = () => window.location.reload();
 
-var llamadoTexto = () => {
-    
-    var texto1 = document.createTextNode("hola pinche putita" + "\n");
-    llamadoTexto1.appendChild(texto1);
+    function mostrar_form1() {
+      if (form.style.display == "none") {
+        form.style.display = "block";
+      } else {
+        form.style.display = "none";
+      }
+    }
 
-};
-
-
-//Esta parte es para manejar el bom, o sea la ventana del navegador
-var recargaPagina = () => window.location.reload();
+    function calcando_texto (){
+      var calca_recoge = nombre.value;
+      llamadoTexto1.innerHTML = calca_recoge;
+    };
+});// fin de onload
 
 
