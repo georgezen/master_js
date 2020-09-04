72064aa26074d2752f11a08af5edf48f7e1756cc
georgezen <georgeutm2014@gmail.com>
2020-09-03 21:37:17 -0500

    juego dardos estilizado,mejorado y con css, terminado alv

--- el-dom-2/js/dardos.js
+++ el-dom-2/js/dardos.js
@@ -6,30 +6,68 @@ var puntaje = 500;
 var ronda = [];
 var dardo_puntos = 0;
 var resultados = document.querySelector(".resultados");
+var buton = document.querySelector("#buton");
+var new1 = document.querySelector("#new");
+var contador = 0;
 
-while (puntaje > 0) {
-  for (let index = 0; index <= 2; index++) {
-    dardo_puntos = parseInt(prompt("dame tu puntaje"));
+buton.addEventListener('click', () => ronda_dardos());
+new1.addEventListener("click", () => new_game());
 
-    while (
-      dardo_puntos <= 0 ||
-      dardo_puntos > 100 ||
-      isNaN(dardo_puntos) ||
-      puntaje - dardo_puntos < 0
-    ) {
-      dardo_puntos = parseInt(prompt("dame tu puntaje correcto pendejo"));
+function ronda_dardos() {
+
+  
+  if (puntaje > 0) {
+    
+    for (let index = 0; index <= 2; index++) {
+      dardo_puntos = parseInt(prompt("dame tu puntaje"));
+
+      while (
+        dardo_puntos <= 0 ||
+        dardo_puntos > 100 ||
+        isNaN(dardo_puntos) ||
+        puntaje - dardo_puntos < 0
+      ) {
+        dardo_puntos = parseInt(prompt("dame tu puntaje correcto pendejo"));
+      }
+
+      puntaje -= dardo_puntos;
+      
+
+      if (puntaje == 0) {
+        ronda.push(dardo_puntos);
+        break;
+      }
+      ronda.push(dardo_puntos);
+
+      console.log(ronda);
+      console.log("tu puntaje restante es: " + puntaje);
+    }
+    contador = contador + 1;
+
+    resultados.innerHTML += "tu puntaje restante es: " + puntaje + "<br>";
+    resultados.innerHTML += "Tus puntos de los dardos son: " + ronda + "<br>";
+    resultados.innerHTML += "Has concluido la ronda: " + contador + "<br>";
+
+    if (puntaje == 0) {
+      resultados.innerHTML += "juego terminado ";
+      buton.style.display = "none";
+      new1.style.display = 'block';
+      contador = 0;
     }
 
-    puntaje -= dardo_puntos;
-    ronda.push(dardo_puntos);
-
-    console.log(ronda);
-    console.log("tu puntaje restante es: " + puntaje);
+    ronda = [];
+    
   }
-
-  resultados.innerHTML += "tu puntaje restante es: " + puntaje + "<br>";
-  resultados.innerHTML += "Tus puntos de los dardos son: " + ronda + "<br>";
-
-  ronda = [];
 }
-resultados.innerHTML += "juego terminado ";
+
+  
+var new_game = () => { 
+  resultados.innerHTML = "";
+  puntaje = 500;
+  resultados.innerHTML += "Tu puntaje ha sido reseteado"+ "<br>";
+  buton.style.display = "block";
+  new1.style.display = "none";
+};
+
+
+
